ea15651ef451ab548473407ec03c9cbd63aa1ee1
thaer <thaer1994.tm98@yahoo.com>
2020-04-05 16:00:17 +0300

    base files

---
+++ server.js
@@ -0,0 +1,15 @@
+// 'use strict';
+// require('dotenv').config();
+
+
+
+// const expreess =require('express');
+// const cors = require('cors');
+
+// const PORT = process.env.PORT || 4000
+// const app = expreess();
+// app.use(cors());
+
+// app.get('/location' , (reuest,response) => {
+
+// });
